fb241f1138df72a153e3f5aac6f4da10d6f9665c
yanggm <1023348836@qq.com>
2021-07-02 17:19:17 +0800

    集成nacos

---
+++ nacosdiscovery/src/main/java/com/yang/RunApplication.java
@@ -0,0 +1,17 @@
+package com.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: RunApplication
+ * @Auther: yanggm
+ * @Date:2021/7/2 14:21
+ * @Description:
+ */
+@SpringBootApplication
+public class RunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class,args);
+    }
+}
---
+++ nacosdiscovery/src/main/java/com/yang/controller/ConfigController.java
@@ -0,0 +1,26 @@
+package com.yang.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: ConfigController
+ * @Auther: yanggm
+ * @Date:2021/7/2 14:13
+ * @Description:
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
